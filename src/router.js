f8f182ed14183c5ef7f439b0be8e0948de70592e
qingjin <122274389@qq.com>
2021-07-06 10:15:40 +0800

    修复错误

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "./pages/Home.vue";
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: "/",
       name: "home",
-      component: () => import("./pages/Home.vue"),
+      component: Home
     },
   ],
 });
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import vue from "@vitejs/plugin-vue";
 import { name } from "./package.json";
 import { resolve } from "path";
+function camelize(str) {
+  return (str + "").replace(/-\D/g, function (match) {
+    return match.charAt(1).toUpperCase();
+  });
+}
 export default ({ command }) => {
   /**
    * @type {import('vite').UserConfig}
@@ -20,12 +25,17 @@ export default ({ command }) => {
         entry: resolve(__dirname, "src/module.js"),
         // 格式必须为iife
         formats: ["iife"],
-        name: name,
+        name: camelize(name),
       },
       minify: false,
       rollupOptions: {
         // 为了使用同一vue对象，所有模块必须外置化vue
         external: ["vue"],
+        output: {
+          globals: {
+            vue: "vue",
+          },
+        },
       },
     };
   }
